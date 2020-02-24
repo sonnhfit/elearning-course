c3bc8b767e46c31c7388486a9d7748c9c5542f9e
Nam Tran <nam_tran@iuj.ac.jp>
2020-02-24 21:24:39 +0900

    Tao Courses Lessons

--- apps/course/admin.py
+++ apps/course/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Courses, Lessons
 # Register your models here.
+admin.site.register(Courses)
+admin.site.register(Lessons)
--- apps/course/models.py
+++ apps/course/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Courses(models.Model):
+    course_name = models.CharField(max_length=200)
+    time_pub = models.DateTimeField()
+
+class Lessons(models.Model):
+    course_name = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    lesson = models.CharField(max_length=100)
+    
